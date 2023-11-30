6a2cd71480abe3432df01efc087e629a32dada82
Viktor Ariel <98894099+viktor-ariel@users.noreply.github.com>
2023-11-29 23:09:31 -0300

    Add files via upload

---
+++ 113/app.py
@@ -0,0 +1,22 @@
+import os
+
+# para saber onde tem mais bibliotetas usar o site pypi existem vários
+
+#criando diretorios
+os.mkdir('Videos')
+os.makedirs('Musicas' + os.sep + 'Ação')
+
+print(os.path.isdir('Musicas'))
+
+########################################################
+#criar ler e modificar aruivos
+with open('celulares.txt', 'w') as arquivo:
+    arquivo.write('Olá meu nome é Viktor')
+
+nomes =['viktor','ana','maria']
+with open('nomes.txt' ,'a', newline='') as arquivo:
+    for nome in nomes:
+        arquivo.write(nome + os.linesep)
+
+
+# r le e r+ lê e escreve
---
+++ 113/shutil.py
@@ -0,0 +1,9 @@
+import shutil
+import os
+
+shutil.copy() #usado para copiar 
+shutil.copytree()#copiar uma pasta interira
+shutil.move ()#usar para mover
+shutil.rmtree() #remover pasta
+shutil.make_archive()#para um zip da pasta
+shutil.unpack_archive()#usado para deszipar o arquivo
